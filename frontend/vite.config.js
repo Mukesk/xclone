841c417d851552d52bb6e2d7fc8a40f4b573156f
Mukeshkanna <94042285+Mukesk@users.noreply.github.com>
2025-02-27 19:07:12 +0530

    ci

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Use "/" if deploying to a custom domain
+  base: "/", // Important for Vercel
   build: {
-    outDir: "dist/", // Ensure build output goes to "dist/"
+    outDir: "dist",
     emptyOutDir: true,
+    assetsDir: "assets",
   }
 });
