1526972eee4c9c02b78c3cf6418b8944176d01d2
Mukeshkanna <94042285+Mukesk@users.noreply.github.com>
2025-04-05 06:06:46 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
+  },
+  server: {
+    historyApiFallback: true,
   }
 });
