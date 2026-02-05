1a85a0e2dfabee4d4abf5ae58529d5f7c643ff7d
Mukeshkanna <mukeshkanna.k2005@gmail.com>
2026-02-05 19:27:10 +0530

    fix: Exclude self from suggestions for logged-in users

--- backend/router/user.route.js
+++ backend/router/user.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import protectorcookie from "../middleware/protectorcookie.js"
+import optionalAuth from "../middleware/optionalAuth.js"
 import { followingRequest, searchProfile, suggested, updateProfile } from "../controller/user.control.js"
 const userRoute = express.Router()
 userRoute.get("/profile/:username", protectorcookie, searchProfile)
 userRoute.post("/follow/:id", protectorcookie, followingRequest)
-userRoute.get("/suggestion", suggested)
+userRoute.get("/suggestion", optionalAuth, suggested)
 userRoute.post("/updateProfile", protectorcookie, updateProfile)
 
 
