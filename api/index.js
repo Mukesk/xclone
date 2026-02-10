804261eb4f1450d9d458a77beac1167d3f1887a5
Mukeshkanna <mukeshkanna.k2005@gmail.com>
2026-02-10 11:56:30 +0530

    chore: remove node_modules and other ignored files from git tracking

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import app from '../backend/server.js';
-
-export default function handler(req, res) {
-    // Vercel serverless function entry point
-    // It passes the request to the express app
-    return app(req, res);
-}
